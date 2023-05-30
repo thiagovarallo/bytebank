6241e56e870a21db9802af8e14c65c2fcbd2e966
Thiago Varallo <thiagovaralllo@gmail.com>
2023-05-29 21:33:29 -0300

    Simulando evento no button

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -3,21 +3,32 @@ import userEvent from "@testing-library/user-event";
 import Formulario from "./index"
 import { type } from "@testing-library/user-event/dist/type";
 
-test('Renderizar um campo do input', () => {
-    render(<Formulario />);
-    const campoText = screen.getByPlaceholderText('Digite um valor');
-    expect(campoText).toBeInTheDocument();
+describe('Renderizar um campo do input', () => {
+    test('', () => {
+        render(<Formulario />);
+        const campoText = screen.getByPlaceholderText('Digite um valor');
+        expect(campoText).toBeInTheDocument();
+    })
+    
+    test('do tipo number', () => {
+        render(<Formulario />)
+        const campotype = screen.getByPlaceholderText('Digite um valor');
+        expect(campotype).toHaveAttribute('type', 'number')
+    })
+    
+    test('Preencher um campo de input', () => {
+        render(<Formulario />)
+        const campotypeNumber = screen.getByPlaceholderText('Digite um valor');
+        userEvent.type(campotypeNumber, '50');
+        expect(campotypeNumber).toHaveValue(50)
+    })
 })
 
-test('Renderizar um campo do tipo number', () => {
-    render(<Formulario />)
-    const campotype = screen.getByPlaceholderText('Digite um valor');
-    expect(campotype).toHaveAttribute('type', 'number')
-})
+test('chamar um evento de submit dentro do button', () => {
+    const realizarTransacao =jest.fn()
 
-test('Preencher um campo de input', () => {
-    render(<Formulario />)
-    const campotypeNumber = screen.getByPlaceholderText('Digite um valor');
-    userEvent.type(campotypeNumber, '50');
-    expect(campotypeNumber).toHaveValue(50)
+    render(<Formulario realizarTransacao={realizarTransacao} />)
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    expect(realizarTransacao).toHaveBeenCalledTimes(1)
 })
